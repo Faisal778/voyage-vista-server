be057d3b88b794f5c381a570750d9622894612f7
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-04-30 13:38:39 +0600

    data in all tourist section and mylist section

--- index.js
+++ index.js
@@ -35,6 +35,20 @@ async function run() {
         res.send(result);
     })
 
+    //my list
+    app.get('/myTouristSpot/:email', async(req, res) => {
+        console.log(req.params.email);
+        const result = await spotCollection.find({email:req.params.email}).toArray();
+        res.send(result)
+    })
+
+    //All Tourists spots
+    app.get('/myTouristSpot', async(req, res) => {
+        const result = await spotCollection.find().toArray();
+        res.send(result)
+    })
+
+
 
 
 
