c474d38db0c9d4ae2db183cc3741cefdbdcbbd7d
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-04-30 19:26:04 +0600

    delete function added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require ('dotenv').config();
 //middleware 
 
@@ -50,6 +50,12 @@ async function run() {
 
 
 
+    app.delete('/myTouristSpot/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result  = await spotCollection.deleteOne(query)
+        res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
